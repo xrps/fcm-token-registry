9578cb9253e55f69b1569a98f284c1938ce9ad7c
Francis Brito <fr.br94@gmail.com>
2018-05-04 17:16:24 -0400

    Add endpoint to retrieve all registered entries.

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const isMain = require('is-main');
+const httpStatus = require('http-status');
 
-function createTokenRegistry() {
-  return express();
+const routes = Object.freeze({
+  ENTRIES: '/',
+});
+
+function factory({ entryStorage }) {
+  const app = express();
+
+  app.get(routes.ENTRIES, (req, res, next) => {
+    entryStorage
+      .getAllEntries()
+      .then(entries => res.status(httpStatus.OK).json(entries))
+      .catch(next);
+  });
+
+  return app;
 }
 
-module.exports = createTokenRegistry;
+exports.factory = factory;
+exports.routes = routes;
 
 if (isMain(module)) {
   const port = process.env.NODE_PORT || 3000;
 
-  createTokenRegistry().listen(port);
+  factory().listen(port);
 
   console.log(`listening at port ${port}`);
 }
--- index.test.js
+++ index.test.js
@@ -1,12 +1,33 @@
 const test = require('ava');
 const isExpressApp = require('is-express-app');
+const supertest = require('supertest');
+const httpStatus = require('http-status');
 
-const createTokenRegistry = require('./');
+const { factory: createTokenRegistry, routes } = require('./');
 
 const isFunction = fn => typeof fn === 'function';
 
-// just a dummy test...
 test('returns an express app factory', (t) => {
   t.true(isFunction(createTokenRegistry));
-  t.true(isExpressApp(createTokenRegistry()));
+  t.true(isExpressApp(createTokenRegistry({ entryStorage: {} })));
 });
+
+test('allows retrieving all entries', async (t) => {
+  const expectedEntries = [
+    { owner: 'cecilia@xrps.co', token: 'foobarbazqnx' },
+    { owner: 'angelica@xrps.co', token: 'qux123456' },
+  ];
+  const entryStorage = {
+    getAllEntries: async () => expectedEntries,
+    saveEntry: async () => {},
+  };
+  await supertest(createTokenRegistry({ entryStorage }))
+    .get(routes.ENTRIES)
+    .expect(httpStatus.OK)
+    .then((response) => {
+      t.deepEqual(response.body, expectedEntries);
+    });
+});
+
+test.todo('allows retrieving all entries by a given group id');
+test.todo('allows registering a new entry to a given group id');
